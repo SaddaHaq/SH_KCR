3ab297838b28d9ab37a7425f53583c35ff0aa6b4
venugopaltattari <venu.tattari@viveninfomedia.com>
2015-01-28 23:13:46 +0530

    Moving submenu in /the_cm to submenu_thecm.php

---
+++ mvc/application/layout/submenu_thecm.php
@@ -0,0 +1,58 @@
+<a id="main-mnu" class="main-mnu" href="#"><img src="<?=DOMAIN?>/assets/img/logo_kcr.png" width="100" height="100" /></a>
+        <div id="ad-mnu" class="ad-mnu transition">
+            <h3 class="men-title">The CM-Initiatives</h3>
+            <section>
+                <div class="diamond" data-tag="My policies towards a developed Telangana">
+                    <a href="<?=DOMAIN?>/the_cm/initiatives">
+                        <div class="in-dmnd transition in"></div>
+                        <div class="txt-dmnd">
+                            <div class="txt">Initiatives</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="diamond" data-tag="Photos and videos">
+                    <a href="<?=DOMAIN?>/the_cm/albums">
+                        <div class="in-dmnd transition in"></div>
+                        <div class="txt-dmnd">
+                            <div class="txt">Gallery</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="diamond" data-tag="My take on people, policies and politics">
+                    <a href="<?=DOMAIN?>/the_cm/blog">
+                        <div class="out-dmnd"></div>
+                        <div class="in-dmnd transition in"></div>
+                        <div class="txt-dmnd">
+                            <div class="txt">Blog</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="diamond" data-tag="Donate for a better Telangana">
+                    <a href="http://cm.telangana.gov.in/" target="_blank">
+                        <div class="in-dmnd transition in"></div>
+                        <div class="txt-dmnd adj-top">
+                            <div class="txt">CM <br> Relief Fund</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="diamond" data-tag="My journey from a common man to a freedom fighter">
+                    <a href="<?=DOMAIN?>/the_cm/news">
+                        <div class="out-dmnd"></div>
+                        <div class="in-dmnd transition in"></div>
+                        <div class="txt-dmnd">
+                            <div class="txt">News</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="diamond shw-top-mnu" id="shw-top-mnu">
+                    <div class="out-dmnd"></div>
+                    <a href="<?=DOMAIN?>">
+                        <div class="in-dmnd transition in"></div>
+                        <div class="txt-dmnd adj-top">
+                            <div class="txt">Back<br>to home</div>
+                        </div>
+                    </a>
+                </div>
+            </section>
+            <p class="tag"></p>
+        </div>
